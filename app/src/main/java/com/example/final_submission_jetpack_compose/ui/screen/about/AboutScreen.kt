8218b48ce632cd789dc95864783fa23835c970ae
akbarz <barz.develop@gmail.com>
2025-03-30 22:14:14 +0700

    add: about page

--- app/src/main/java/com/example/final_submission_jetpack_compose/ui/screen/about/AboutScreen.kt
+++ app/src/main/java/com/example/final_submission_jetpack_compose/ui/screen/about/AboutScreen.kt
@@ -1,20 +1,39 @@
 package com.example.final_submission_jetpack_compose.ui.screen.about
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.final_submission_jetpack_compose.R
+import com.example.final_submission_jetpack_compose.ui.theme.SoftWhite
 
 
 @Composable
@@ -24,14 +43,44 @@ fun AboutScreen(
     Scaffold(
         topBar = { AppBar(navigateBack) },
         content = { padding ->
-            Box(
+            Column(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(padding),
-                contentAlignment = Alignment.Center
+                    .padding(padding)
+                    .background(SoftWhite),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text("About Page")
+                Spacer(modifier = Modifier.height(40.dp))
+
+                Image(
+                    painter = painterResource(R.drawable.photo),
+                    contentDescription = "Profile Photo",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(150.dp)
+                        .border(
+                            BorderStroke(4.dp, MaterialTheme.colorScheme.primary.copy(alpha = 0.7f)),
+                            CircleShape
+                        )
+                        .padding(4.dp)
+                        .clip(CircleShape)
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+                Text(
+                    text = stringResource(R.string.creator_name),
+                    style = MaterialTheme.typography.titleLarge,
+                    color = MaterialTheme.colorScheme.primary,
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = stringResource(R.string.creator_email),
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.primary,
+                )
             }
+
+
         }
     )
 }
@@ -42,7 +91,7 @@ fun AppBar(
     navigateBack: () -> Unit,
 ) {
     CenterAlignedTopAppBar(
-        title = { Text("About") },
+        title = { Text("About Me") },
         navigationIcon = {
             IconButton(onClick = navigateBack) {
                 Icon(
