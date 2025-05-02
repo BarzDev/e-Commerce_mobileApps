31f5a2d81c8f34edf0117d0b36cfd63ae1326d7e
akbarz <barz.develop@gmail.com>
2025-05-02 20:48:18 +0700

    fix: search bar component

--- app/src/main/java/com/example/final_submission_jetpack_compose/ui/components/SearchBar.kt
+++ app/src/main/java/com/example/final_submission_jetpack_compose/ui/components/SearchBar.kt
@@ -1,19 +1,17 @@
 package com.example.final_submission_jetpack_compose.ui.components
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,27 +23,27 @@ fun SearchBar(
     onQueryChange: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    val focusManager = LocalFocusManager.current
-
     TextField(
         value = query,
         onValueChange = onQueryChange,
-        label = { Text(stringResource(R.string.search)) },
+        placeholder = { Text(stringResource(R.string.search)) },
         leadingIcon = {
             Icon(
                 imageVector = Icons.Default.Search,
                 contentDescription = "Search Icon",
-                tint = MaterialTheme.colorScheme.onSurface
+                tint = MaterialTheme.colorScheme.primary
             )
         },
         singleLine = true,
-        keyboardOptions = KeyboardOptions.Default,
-        keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() }),
         shape = MaterialTheme.shapes.medium,
+        colors = TextFieldDefaults.colors(
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent
+        ),
         modifier = modifier
             .fillMaxWidth()
             .padding(16.dp)
-            .clickable { }
     )
 }
 
