5084e8388ef9dcb7274b2025eacdd0c5497a2dcc
akbarz <barz.develop@gmail.com>
2025-05-03 19:54:31 +0700

    fix: change profile layout

--- app/src/main/java/com/example/final_submission_jetpack_compose/ui/screen/about/AboutScreen.kt
+++ app/src/main/java/com/example/final_submission_jetpack_compose/ui/screen/about/AboutScreen.kt
@@ -1,20 +1,18 @@
 package com.example.final_submission_jetpack_compose.ui.screen.about
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -23,14 +21,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.final_submission_jetpack_compose.R
 import com.example.final_submission_jetpack_compose.ui.theme.SoftWhite
 
 
@@ -50,41 +42,33 @@ fun AboutScreen(
             ) {
                 Spacer(modifier = Modifier.height(40.dp))
 
-                Image(
-                    painter = painterResource(R.drawable.photo),
-                    contentDescription = "Profile Photo",
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .size(150.dp)
-                        .border(
-                            BorderStroke(
-                                4.dp,
-                                MaterialTheme.colorScheme.primary.copy(alpha = 0.7f)
-                            ),
-                            CircleShape
-                        )
-                        .padding(4.dp)
-                        .clip(CircleShape)
-                )
-                Spacer(modifier = Modifier.height(20.dp))
-                Text(
-                    text = stringResource(R.string.creator_name),
-                    style = MaterialTheme.typography.titleLarge,
-                    color = MaterialTheme.colorScheme.primary,
-                    fontWeight = FontWeight.Bold
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(
-                    text = stringResource(R.string.creator_email),
-                    style = MaterialTheme.typography.titleMedium,
-                    color = MaterialTheme.colorScheme.primary,
-                )
+                ItemProfile("UserID : Id-2457XS9")
+                ItemProfile("Username : John Wick")
+                ItemProfile("Email : jhon_wick@mail.com")
+                ItemProfile("Phone : 0812 3456 7890")
+                ItemProfile("Account : Private account")
             }
-
-
         }
     )
 }
+
+@Composable
+fun ItemProfile(
+    title:String
+){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 32.dp, vertical = 8.dp)
+    ) {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.titleMedium,
+        )
+    }
+    HorizontalDivider(modifier = Modifier.padding(horizontal = 12.dp))
+}
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -92,7 +76,7 @@ fun AppBar(
     navigateBack: () -> Unit,
 ) {
     CenterAlignedTopAppBar(
-        title = { Text("About Me") },
+        title = { Text("Profile") },
         navigationIcon = {
             IconButton(onClick = navigateBack) {
                 Icon(
